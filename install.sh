3a78f49b249eb5344cef175a4230f688029c0b63
Petri Avikainen <petri.avikainen@gmail.com>
2017-03-20 13:22:35 +0200

    fix permission when installing

--- install.sh
+++ install.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
 install_path=/usr/local/bin
-
 script_dir=$( cd $(dirname $0); pwd -P )
-
 cp "$script_dir/flowdocktee.sh" "$install_path/flowdocktee"
-
-chmod +x "$install_path/$flowdocktee_script"
+chmod +x "$install_path/flowdocktee"
 
 echo "flowdocktee has been installed to $install_path"
